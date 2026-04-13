bed1fcb631855700619cdac01669b1b207ac3535
Issa-hub14 <isabellaquijano1@gmail.com>
2026-04-12 21:27:50 -0500

    Código de la clase Fondista perteneciente al modelo, clase hija

---
+++ src/main/java/Modelo/Fondista.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author isabe
+ */
+public class Fondista extends Competidor{
+    private double distanciaRecorrida;
+
+    public Fondista(String nombre, int edad, String pais, int rankingMundial, double estatura, double peso, double distanciaRecorre) {
+        super(nombre, edad, pais, rankingMundial, estatura, peso);
+        this.distanciaRecorrida = distanciaRecorre;
+    }
+
+    public double getDistanciaRecorre() {
+        return distanciaRecorrida;
+    }
+
+    public void setDistanciaRecorre(double distanciaRecorre) {
+        this.distanciaRecorrida = distanciaRecorre;
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + " | Fondista" +
+               " | Vel. Máx: " + distanciaRecorrida + " km/h";
+    }
+
+    public String obtenerDatos() {
+        return super.obtenerDatos() +
+               "\nTipo: Fondista" +
+               "\nDistancia que recorre: " + distanciaRecorrida + " km";
+    }
+}
