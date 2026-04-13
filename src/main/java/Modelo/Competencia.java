b0f5d193e93ed7c8301f7f3531255baca281f80b
Issa-hub14 <isabellaquijano1@gmail.com>
2026-04-12 22:02:57 -0500

    Código de la clase Competencia del modelo con sus métodos

--- src/main/java/Modelo/Competencia.java
+++ src/main/java/Modelo/Competencia.java
@@ -9,5 +9,37 @@ package Modelo;
  * @author isabe
  */
 public class Competencia {
+    private String nombreEvento;
+    private Equipo[] equipos;
+    private int contador;
     
+    public Competencia(String nombreEvento){
+        this.nombreEvento = nombreEvento;
+    }
+
+    public String getNombreEvento() {
+        return nombreEvento;
+    }
+
+    public void setNombreEvento(String nombreEvento) {
+        this.nombreEvento = nombreEvento;
+    }
+
+    public Equipo[] getEquipos() {
+        return equipos;
+    }
+
+    public void setEquipos(Equipo[] equipos) {
+        this.equipos = equipos;
+    }
+    
+    public void agregarEquipo(Equipo e){
+        if (contador < equipos.length) {
+            equipos[contador] = e;
+            contador++;  
+        }
+    }
+    public String generarReporte(){
+        return equipos.toString();
+    }
 }
