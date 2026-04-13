22f1ff020b2c6c7ac6c7652a64efa444f2f9a789
Issa-hub14 <isabellaquijano1@gmail.com>
2026-04-12 21:56:45 -0500

    Código de la clase Equipo del modelo con sus métodos

--- src/main/java/Modelo/Equipo.java
+++ src/main/java/Modelo/Equipo.java
@@ -9,5 +9,50 @@ package Modelo;
  * @author isabe
  */
 public class Equipo {
+    private String nombre;
+    private String pais;
+    private Competidor[] competidores;
+    private int contador;
     
+    public Equipo(String nombre, String pais){
+        this.nombre = nombre;
+        this.pais = pais;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public Competidor[] getCompetidores() {
+        return competidores;
+    }
+
+    public void setCompetidores(Competidor[] competidores) {
+        this.competidores = competidores;
+    }
+    
+    public void agregarCompetidor(Competidor c){
+        if (contador < competidores.length) {
+            competidores[contador] = c;
+            contador++;  
+        }
+    }
+    public String obtenerDatosEquipo(){
+        return "Equipo: "+ nombre +
+                "\nPais: " + pais +
+                "\nNúmero de competidores: " + contador;
+        
+    }
 }
