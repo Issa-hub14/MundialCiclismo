c264bb506f19937b42ee6bbaaebb95c18d85700d
Issa-hub14 <isabellaquijano1@gmail.com>
2026-04-12 20:54:34 -0500

    Creación del proyecto para el manejo de una competencia de ciclismo

---
+++ src/main/java/com/mycompany/mundialciclismo/MundialCiclismo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mundialciclismo;
+
+/**
+ *
+ * @author isabe
+ */
+public class MundialCiclismo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
